bbf0e4e536f0b89ea72488826461d840b1eb3b1b
opedrodev <pvasilva.dev@hotmail.com>
2022-11-29 17:51:54 -0300

    feat(R09): adiciona toggle na barra de pesquisa

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 function Header(props) {
   const { title } = props;
+  const history = useHistory();
+  const [isSearching, setIsSearching] = useState(false);
 
   // Componentes que não terão o botão de pesquisa
   const noSearchIcon = ['Profile', 'Done Recipes', 'Favorite Recipes'];
@@ -16,15 +19,32 @@ function Header(props) {
 
       {!noSearchIcon.includes(title)
       && (
-        <button type="button">
+        <button
+          type="button"
+          onClick={ () => setIsSearching(!isSearching) }
+        >
           <img src={ searchIcon } alt="search icon" data-testid="search-top-btn" />
         </button>
       )}
 
-      <button type="button">
+      <button
+        type="button"
+        onClick={ () => history.push('/profile') }
+      >
         <img src={ profileIcon } alt="profile icon" data-testid="profile-top-btn" />
       </button>
 
+      {isSearching
+      && (
+        <div>
+          <input
+            type="text"
+            name="search"
+            id="search"
+            data-testid="search-input"
+          />
+        </div>
+      )}
     </header>
   );
 }
