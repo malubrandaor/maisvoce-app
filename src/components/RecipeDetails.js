478b7f7425836f323b54ebd6d1cc1a24825452d3
Gabriel <devpalhares@gmail.com>
2022-12-01 17:31:58 -0300

    merge para prosseguir

--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -7,23 +7,32 @@ function RecipeDetails() {
 
   const [recipe, setRecipe] = useState([]);
 
+  const [mealsRecommendation, setMealsRecommendation] = useState([]);
+  const [drinksRecommendation, setDrinksRecommendation] = useState([]);
+
   const requestApi = async (id, type) => {
     if (type === 'meals') {
       const response = await
       fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
       const responseJson = await response.json();
-      console.log(responseJson.meals[0]);
       setRecipe(responseJson.meals[0]);
-      return responseJson.meals[0];
+
+      const responseDrinks = await
+      fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+      const responseDrinksJson = await responseDrinks.json();
+      setDrinksRecommendation(responseDrinksJson.drinks);
     }
 
     if (type === 'drinks') {
       const response = await
       fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
       const responseJson = await response.json();
-      console.log(responseJson.drinks[0]);
       setRecipe(responseJson.drinks[0]);
-      return responseJson.drinks[0];
+
+      const responseMeals = await
+      fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+      const responseMealsJSON = await responseMeals.json();
+      setMealsRecommendation(responseMealsJSON.meals);
     }
   };
 
@@ -44,14 +53,15 @@ function RecipeDetails() {
     return ingredientsWithMeasures;
   };
 
-  console.log(findIngredients(recipe));
   useEffect(() => {
     requestApi(params.id, history.location.pathname.split('/')[1]);
   }, []);
 
-  //   console.log(params);
-  //   console.log(history.location.pathname.split('/')[1]);
-
+  function embedVideo(url) {
+    const urlEmbed = `https://www.youtube.com/embed/${url.split('https://www.youtube.com/')}`;
+    return urlEmbed;
+  }
+  console.log(embedVideo('https://www.youtube.com/watch?v=1IszT_guI08'));
   return (
     <div>
       <img
@@ -66,6 +76,15 @@ function RecipeDetails() {
         <p data-testid={ `${i}-ingredient-name-and-measure` } key={ i }>
           {`${ingredient.name} ${ingredient.measure}`}
         </p>))}
+
+      {recipe.strYoutube && <iframe
+        width="560"
+        title={ recipe.strMealThumb }
+        height="315"
+        src={ embedVideo(recipe.strYoutube) }
+        frameBorder="0"
+        allowFullScreen
+      />}
 
     </div>
   );
