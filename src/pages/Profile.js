73d72106420fe2f4ac4ffcf60adc562ca51b2df8
flavioramonnunes <f.ramon_paixao@hotmail.com>
2022-12-01 17:24:27 -0300

    feat: Requisito 57: Implementações de Butões.

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -6,6 +6,10 @@ function Profile() {
   return (
     <>
       <Header title="Profile" />
+      <span data-testid="profile-email">...</span>
+      <button type="button" data-testid="profile-done-btn">Done Recipes</button>
+      <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
+      <button type="button" data-testid="profile-logout-btn">Logout</button>
       <Footer />
     </>
   );
