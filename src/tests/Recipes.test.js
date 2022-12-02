7c23fe88a888eb805253cfa644b12cbba15e8300
opedrodev <pvasilva.dev@hotmail.com>
2022-12-02 18:33:02 -0300

    test(R23): adiciona testes

--- src/tests/Recipes.test.js
+++ src/tests/Recipes.test.js
@@ -3,29 +3,18 @@ import userEvent from '@testing-library/user-event';
 import App from '../App';
 import { renderWithRouterAndRedux } from './helpers/renderWith';
 
-beforeEach(() => {
-  cleanup();
-});
-describe('Testes do componente Recipes', () => {
-  it('Verifica se o componente Recipes renderiza uma receita na rota Meals', async () => {
+const RECIPE_CARD_ZERO = '0-recipe-card';
+
+describe('Testa o componente Recipes na rota /meals', () => {
+  beforeEach(() => cleanup());
+
+  it('Verifica se uma receita é renderizada na tela', async () => {
     renderWithRouterAndRedux(<App />, { initialEntries: ['/meals'] });
-
-    // await waitFor(async () => {
-    // }, { timeout: 2000 });
     const recipe = screen.getAllByRole('img')[0];
     await waitFor(() => expect(recipe).toBeInTheDocument(), { timeout: 2000 });
   });
 
-  it('Verifica se o componente Recipes renderiza uma receita na rota Drinks', async () => {
-    renderWithRouterAndRedux(<App />, { initialEntries: ['/drinks'] });
-
-    // await waitFor(async () => {
-    // }, { timeout: 2000 });
-    const recipe = screen.getAllByRole('img')[0];
-    await waitFor(() => expect(recipe).toBeInTheDocument(), { timeout: 2000 });
-  });
-
-  it('Testa os botões de categoria na rota Meals', async () => {
+  it('Verifica os botões de categoria', async () => {
     renderWithRouterAndRedux(<App />, { initialEntries: ['/meals'] });
     const allCategoryButton = screen.getByText('All');
     expect(allCategoryButton).toBeInTheDocument();
@@ -34,33 +23,39 @@ describe('Testes do componente Recipes', () => {
     expect(beefButton).toBeInTheDocument();
     userEvent.click(beefButton);
 
-    let mealCard = await screen.findByTestId('0-recipe-card');
+    let mealCard = await screen.findByTestId(RECIPE_CARD_ZERO);
     await waitFor(() => expect(mealCard).toBeInTheDocument(), { timeout: 2000 });
 
     userEvent.click(allCategoryButton);
 
-    mealCard = await screen.findByTestId('0-recipe-card');
+    mealCard = await screen.findByTestId(RECIPE_CARD_ZERO);
     await waitFor(() => expect(mealCard).toBeInTheDocument(), { timeout: 2000 });
   });
+});
 
-  // it('Testa os botões de categoriana rota Drinks', async () => {
-  //   renderWithRouterAndRedux(<Drinks />);
+describe('Testa o componente Recipes na rota /drinks', () => {
+  it('Verifica se uma receita é renderizada na tela', async () => {
+    renderWithRouterAndRedux(<App />, { initialEntries: ['/drinks'] });
 
-  //   const allCategoryButton = screen.getByRole('button', { name: 'All' });
-  //   expect(allCategoryButton).toBeInTheDocument();
+    const recipe = screen.getAllByRole('img')[0];
+    await waitFor(() => expect(recipe).toBeInTheDocument(), { timeout: 2000 });
+  });
 
-  //   const cocktailButton = screen.getByRole('button', { name: 'Cocktail' });
-  //   expect(cocktailButton).toBeInTheDocument();
-  //   userEvent.click(cocktailButton);
+  it('Verifica a categoria All', async () => {
+    renderWithRouterAndRedux(<App />, { initialEntries: ['/drinks'] });
+    const allCategoryButton = screen.getByRole('button', { name: 'All' });
+    expect(allCategoryButton).toBeInTheDocument();
 
-  //   let drinkCard = await screen.findByTestId('0-recipe-card');
-  //   expect(drinkCard).toBeInTheDocument();
-  //   expect(drinkCard).toHaveTextContent('155 Belmont');
+    const cocktailButton = screen.getByText('Cocktail');
+    expect(cocktailButton).toBeInTheDocument();
+    userEvent.click(cocktailButton);
 
-  //   userEvent.click(allCategoryButton);
+    let drinkCard = await screen.findByTestId(RECIPE_CARD_ZERO);
+    await waitFor(() => expect(drinkCard).toBeInTheDocument(), { timeout: 2000 });
 
-  //   drinkCard = await screen.findByTestId('0-recipe-card');
-  //   expect(drinkCard).toBeInTheDocument();
-  //   expect(drinkCard).toHaveTextContent('GG');
-  // });
+    userEvent.click(allCategoryButton);
+
+    drinkCard = await screen.findByTestId(RECIPE_CARD_ZERO);
+    await waitFor(() => expect(drinkCard).toBeInTheDocument(), { timeout: 2000 });
+  });
 });
