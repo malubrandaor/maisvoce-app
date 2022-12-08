da79d8e5620716fca210ecb70461251a499b58cf
Gabriel <devpalhares@gmail.com>
2022-12-08 16:42:07 -0300

    lint

--- src/tests/RecipeInProgress.test.js
+++ src/tests/RecipeInProgress.test.js
@@ -1,7 +1,6 @@
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
-import { act } from 'react-dom/test-utils';
 import App from '../App';
 import { renderWithRouterAndRedux } from './helpers/renderWith';
 
